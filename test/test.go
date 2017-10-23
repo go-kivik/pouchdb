4661f303cc530377999ddf477e5a3b02f9b8b25a
Jonathan Hall <flimzy@flimzy.com>
2017-10-23 21:01:06 +0200

    Test PouchDB against Couch 2.1

--- test/test.go
+++ test/test.go
@@ -214,7 +214,7 @@ func RegisterPouchDBSuites() {
 
 		"Replicate.NotFoundDB": func() string {
 			var dsn string
-			for _, env := range []string{"KIVIK_TEST_DSN_COUCH16", "KIVIK_TEST_DSN_COUCH20", "KIVIK_TEST_DSN_CLOUDANT"} {
+			for _, env := range []string{"KIVIK_TEST_DSN_COUCH21", "KIVIK_TEST_DSN_COUCH20", "KIVIK_TEST_DSN_COUCH16", "KIVIK_TEST_DSN_CLOUDANT"} {
 				dsn = os.Getenv(env)
 				if dsn != "" {
 					break
@@ -254,5 +254,5 @@ func PouchLocalTest(t *testing.T) {
 
 // PouchRemoteTest runs the PouchDB tests against a remote CouchDB database.
 func PouchRemoteTest(t *testing.T) {
-	kiviktest.DoTest(kiviktest.SuitePouchRemote, "KIVIK_TEST_DSN_COUCH20", t)
+	kiviktest.DoTest(kiviktest.SuitePouchRemote, "KIVIK_TEST_DSN_COUCH21", t)
 }
