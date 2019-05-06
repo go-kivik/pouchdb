2f81c4dc743ebd701f24891eaa11ca5ef65478a5
Jonathan Hall <flimzy@flimzy.com>
2019-05-06 18:58:29 +0200

    Reformat pouchdb.inc.js

--- bindings/pouchdb.inc.js
+++ bindings/pouchdb.inc.js
@@ -1,18 +1,18 @@
-if ( $global.PouchDB === undefined ) {
-    try {
-        $global.PouchDB = require('pouchdb');
-    } catch(e) {
-        throw("kivik: pouchdb bindings: Cannot find global PouchDB object. Did you load the PouchDB library?");
-    }
+if ($global.PouchDB === undefined) {
+  try {
+    $global.PouchDB = require("pouchdb");
+  } catch (e) {
+    throw "kivik: pouchdb bindings: Cannot find global PouchDB object. Did you load the PouchDB library?";
+  }
 }
 try {
-    require('pouchdb-all-dbs')($global.PouchDB);
-} catch(e) {}
+  require("pouchdb-all-dbs")($global.PouchDB);
+} catch (e) {}
 
 try {
-    $global.PouchDB.plugin(require('pouchdb-find'));
-} catch(e) {}
+  $global.PouchDB.plugin(require("pouchdb-find"));
+} catch (e) {}
 
 try {
-    global.XMLHttpRequest = require('xhr2');
-} catch(e) {}
+  global.XMLHttpRequest = require("xhr2");
+} catch (e) {}
